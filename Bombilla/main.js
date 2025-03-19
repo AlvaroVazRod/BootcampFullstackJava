3391214d22208c4fa388f87b6321054284c0c512
Alvaro Vazquez <alvarovazrod@gmail.com>
2025-03-19 15:44:38 +0100

    Bombilla mejora inputs

--- Bombilla/main.js
+++ Bombilla/main.js
@@ -5,6 +5,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const bulbImg = document.getElementById("bulb");
     const brightnessRange = document.getElementById("brightnessRange");
     const sizeBulb = document.getElementById("sizeImage");
+    const turnBulb = document.getElementById("turnImage");
+    const angleBulb = document.getElementById("angleValue");
+    bulbImg.style.transition = "transform 0.5s ease-in-out";
     if (bulbImg && switchImg) {
         switchImg.addEventListener("click", (event) => {
             const isOff = bulbImg.classList.contains("off");
@@ -21,6 +24,19 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     }
+    turnBulb.addEventListener("input", (event) => {
+        const target = event.target;
+        const turnImg = `rotate(${target.value}deg)`;
+        angleBulb.innerText = `${target.value}°`;
+        bulbImg.style.transform = turnImg;
+    });
+    turnBulb.addEventListener("change", () => {
+        setTimeout(() => {
+            turnBulb.value = "0";
+            bulbImg.style.transform = "rotate(0deg)";
+            angleBulb.innerText = "0";
+        }, 500);
+    });
     sizeBulb.addEventListener("input", (event) => {
         const target = event.target;
         const sizeImg = target.value + "px";
--- Bombilla/main.ts
+++ Bombilla/main.ts
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const bulbImg = document.getElementById("bulb") as HTMLImageElement;
     const brightnessRange = document.getElementById("brightnessRange") as HTMLInputElement;
     const sizeBulb = document.getElementById("sizeImage") as HTMLInputElement;
+    const turnBulb = document.getElementById("turnImage") as HTMLInputElement;
+    const angleBulb = document.getElementById("angleValue") as HTMLSpanElement;
+    bulbImg.style.transition = "transform 0.5s ease-in-out";
     if (bulbImg && switchImg) {
         switchImg.addEventListener("click", (event: MouseEvent) => {
             const isOff = bulbImg.classList.contains("off");
@@ -20,14 +23,27 @@ document.addEventListener("DOMContentLoaded", () => {
         });
 
     }
-    sizeBulb.addEventListener("input", (event) =>{
+    turnBulb.addEventListener("input", (event) => {
+        const target = event.target as HTMLInputElement;
+        const turnImg = `rotate(${target.value}deg)`;
+        angleBulb.innerText = `${target.value}°`;
+        bulbImg.style.transform = turnImg;
+    });
+    turnBulb.addEventListener("change", () => {
+        setTimeout(() => {
+            turnBulb.value = "0";
+            bulbImg.style.transform = "rotate(0deg)";
+            angleBulb.innerText = "0";
+        }, 500);
+    })
+    sizeBulb.addEventListener("input", (event) => {
         const target = event.target as HTMLInputElement;
         const sizeImg = target.value + "px";
-        bulbImg.style.width =  sizeImg;
+        bulbImg.style.width = sizeImg;
         bulbImg.style.height = sizeImg;
     });
     brightnessRange.addEventListener("input", (event) => {
-        if (bulbImg.classList.contains("on")) { 
+        if (bulbImg.classList.contains("on")) {
             const target = event.target as HTMLInputElement;
             const brightnessValue = target.value;
             bulbImg.style.filter = `brightness(${brightnessValue})`;
